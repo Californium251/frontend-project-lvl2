fa4570a6dba23f3fc60a9678cd711d188a444741
Maxim Kurepov <maximkurepov@Maxims-MacBook-Pro.local>
2022-07-22 19:37:06 +0300

    доработал тесты

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,7 +6,7 @@ import genDiff from '../src/compare.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('test Jest', () => {
+test('test initial files in the task', () => {
   expect(genDiff(`${__dirname}/../src/file2.json`, `${__dirname}/../src/file1.json`)).toStrictEqual({
     '+ follow': false,
     '  host': 'hexlet.io',
@@ -14,5 +14,32 @@ test('test Jest', () => {
     '- timeout': 20,
     '+ timeout': 50,
     '- verbose': true,
+  });
+});
+
+test('test same files', () => {
+  expect(genDiff(`${__dirname}/test_file_1.1.json`, `${__dirname}/test_file_1.2.json`)).toStrictEqual({
+    '  host': 'hexlet.io',
+    '  timeout': 50,
+    '  proxy': '123.234.53.22',
+    '  follow': false,
+  });
+});
+
+test('test files with no overlap', () => {
+  expect(genDiff(`${__dirname}/test_file_2.1.json`, `${__dirname}/test_file_2.2.json`)).toStrictEqual({
+    '- follow': 'false',
+    '+ host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '+ timeout': '50',
+  });
+});
+
+test('test files with diferent values', () => {
+  expect(genDiff(`${__dirname}/test_file_3.1.json`, `${__dirname}/test_file_3.2.json`)).toStrictEqual({
+    '- host': 'hexlet.io',
+    '+ host': 'hexlet.ru',
+    '- timeout': '50',
+    '+ timeout': 30,
   });
 });
