fd1de541ceabf8bb799eecb5b11bf9bf4bf9f93f
Maxim Kurepov <maximkurepov@Maxims-MacBook-Pro.local>
2022-07-22 19:18:05 +0300

    set up jest in github actions

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import genDiff from '../src/compare.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 test('test Jest', () => {
-  expect(genDiff(`${__dirname}/src/file2.json`, `${__dirname}/src/file1.json`)).toStrictEqual({
+  expect(genDiff(`${__dirname}/../src/file2.json`, `${__dirname}/../src/file1.json`)).toStrictEqual({
     '+ follow': false,
     '  host': 'hexlet.io',
     '+ proxy': '123.234.53.22',
