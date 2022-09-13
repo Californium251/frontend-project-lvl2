5f884b533b4890c51be58c3d379c1a117098d87e
Maxim Kurepov <maximkurepov@Maxims-MacBook-Pro.local>
2022-09-13 21:17:40 +0300

    Поменял названия файлов тестов, поменял код, чтобы файлы фикстур подгружались один раз. Также убрал обёртку функции и экспорт в исполняемом файле

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,23 +7,25 @@ import genDiff from '../src/compare.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const expextedResult1 = readFileSync(getFixturePath('expected_1.txt'), 'utf-8');
+const expectedResult2 = readFileSync(getFixturePath('expected_2.txt'), 'utf-8');
 
 test('test files from the task on Hexlet', () => {
-  expect(genDiff(getFixturePath('test_file_1.1.json'), getFixturePath('test_file_1.2.json')))
-    .toStrictEqual(readFileSync(getFixturePath('test_expected_result_1.txt'), 'utf-8'));
+  expect(genDiff(getFixturePath('test_1.json'), getFixturePath('test_2.json')))
+    .toStrictEqual(expextedResult1);
 });
 
 test('test YAML files from the task on Hexlet', () => {
-  expect(genDiff(getFixturePath('test_file_1.1.yml'), getFixturePath('test_file_1.2.yml')))
-    .toStrictEqual(readFileSync(getFixturePath('test_expected_result_1.txt'), 'utf-8'));
+  expect(genDiff(getFixturePath('test_1.yml'), getFixturePath('test_2.yml')))
+    .toStrictEqual(expextedResult1);
 });
 
 test('test plain format', () => {
-  expect(genDiff(getFixturePath('test_file_1.1.yml'), getFixturePath('test_file_1.2.yml'), 'plain'))
-    .toStrictEqual(readFileSync(getFixturePath('test_expected_result_2.txt'), 'utf-8'));
+  expect(genDiff(getFixturePath('test_1.yml'), getFixturePath('test_2.yml'), 'plain'))
+    .toStrictEqual(expectedResult2);
 });
 
 test('test JSON format', () => {
-  expect(genDiff(getFixturePath('test_file_1.1.yml'), getFixturePath('test_file_1.2.yml'), 'json'))
-    .toStrictEqual(readFileSync(getFixturePath('test_expected_result_3.json'), 'utf-8'));
+  expect(genDiff(getFixturePath('test_1.yml'), getFixturePath('test_2.yml'), 'json'))
+    .toStrictEqual(expectedResult2);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,19 +5,14 @@ import compare from '../src/compare.js';
 
 const program = new Command();
 
-const genDiff = () => {
-  program
-    .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.')
-    .argument('<first file path>', 'first file path')
-    .argument('<second file path>', 'second file path')
-    .option('-f, --format <type>', 'output format')
-    .action((firstFilePath, secondFilePath) => {
-      const { format } = program.opts();
-      console.log(compare(firstFilePath, secondFilePath, format));
-    })
-    .parse(process.argv);
-};
-
-genDiff();
-export default genDiff;
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<first file path>', 'first file path')
+  .argument('<second file path>', 'second file path')
+  .option('-f, --format <type>', 'output format')
+  .action((firstFilePath, secondFilePath) => {
+    const { format } = program.opts();
+    console.log(compare(firstFilePath, secondFilePath, format));
+  })
+  .parse(process.argv);
